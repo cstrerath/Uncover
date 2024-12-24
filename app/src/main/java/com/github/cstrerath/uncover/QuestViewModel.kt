2577fc59abc843705d1e56cfa19eafa1e3a765d0
cstrerath <salzstreri@gmail.com>
2024-12-24 16:20:34 +0100

    fix: add missing parts from previous commit

---
+++ app/src/main/java/com/github/cstrerath/uncover/QuestViewModel.kt
@@ -0,0 +1,26 @@
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.github.cstrerath.uncover.AppDatabase
+import com.github.cstrerath.uncover.Quest
+import com.github.cstrerath.uncover.QuestStep
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class QuestViewModel(application: Application) : AndroidViewModel(application) {
+    private val _quests = MutableStateFlow<List<Quest>>(emptyList())
+    val quests = _quests.asStateFlow()
+
+    private val _questSteps = MutableStateFlow<List<QuestStep>>(emptyList())
+    val questSteps = _questSteps.asStateFlow()
+
+    fun loadQuestsForCharacter(questId: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val database = AppDatabase.getInstance(getApplication())
+            _quests.value = database.questDao().getAllQuests()
+            _questSteps.value = database.questStepDao().getStepsForQuest(questId)
+        }
+    }
+}
